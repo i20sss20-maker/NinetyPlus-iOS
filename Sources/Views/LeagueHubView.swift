f17ed7e08ba5b94e6a2d7b855871a6357be12bd0
i20sss20-maker <i20sss20@gmail.com>
2026-09-07 05:27:54 +0300

    Remove unused legacy LeagueHub wrapper

--- Sources/Views/LeagueHubView.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-// Legacy compatibility wrapper. Production league pages use V2 + Railway.
-struct LeagueHubView: View {
-    let league: LeagueOption
-    var body: some View { V2LeagueHubView(league: league) }
-}
